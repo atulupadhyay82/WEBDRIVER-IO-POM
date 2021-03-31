5c494a29b7328fe196d7874eed93964a54aace58
swatigaur123 <gaurcs123@gmail.com>
2021-03-30 21:25:39 -0400

    Added retry logic

--- test/login.page.test.js
+++ test/login.page.test.js
@@ -3,19 +3,24 @@ const loginPage= require("../pages/login.page")
 const configData= require("../config")
 const constant= require("../constant")
 
-describe("login page feature test", () => {
-    this.retries(2)
-    it('verify login page title', () => {
+/**
+ * If you want to use Mocha features via this you need to use unbound functions not arrow functions, 
+ * as documented: https://mochajs.org/#arrow-functions
+ */
+describe("login page feature test", function(){
+    this.retries(2);
+    it('verify login page title', function() {
         browser.url('./');
         browser.maximizeWindow();
         const title=loginPage.getPageTitle();
         console.log("login page title is "+title);
         assert.equal(constant.LOGIN_PAGE_TITLE,title,'title is not found');
     }),
-    it('verify sign up link', () => {
+    it.only('verify sign up link', function() {
+        browser.url('./');
         assert.equal(true, loginPage.isSignUpLinkExist(),'Sign up link is not present');
     }),
-    it('verify login', () => {
+    it('verify login', function() {
         loginPage.doLogin(configData.username,configData.password);
         //assert.equal(true, loginPage.isSignUpLinkExist(),'Sign up link is not present');
     })
